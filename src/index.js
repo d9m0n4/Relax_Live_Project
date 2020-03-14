792a6cd7f7d07ccd95ec2fab6c59827882f8e5bc
user <c2h8e9d4@gmail.com>
2020-03-14 16:52:56 +0300

    маска телефона

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import showNumTel from './modules/showNumTel';
 import mainMenu from './modules/mainMenu';
 import srcollFromMenu from './modules/scrollTo';
 import popup from './modules/popupRepair';
+import maskPhone from './plugins/maskPhone';
 showNumTel();
 mainMenu();
 srcollFromMenu();
 popup();
+maskPhone();
 
 
 
