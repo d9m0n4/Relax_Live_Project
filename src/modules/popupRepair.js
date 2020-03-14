5be6b2c36739177e411a3e9dd0293c544c8fe7fc
user <c2h8e9d4@gmail.com>
2020-03-14 18:22:25 +0300

    модальные окна

--- src/modules/popupRepair.js
+++ src/modules/popupRepair.js
@@ -1,13 +1,27 @@
 const popup = () => {
-    const popupBlock = document.querySelector('.popup-repair-types');
-    document.body.addEventListener('click', e => {
-        const target = e.target;
+    const popupRepair = document.querySelector('.popup-repair-types');
+    const popupPrivacy = document.querySelector('.popup-privacy');
+
+    const popups = (popupType, target) => {
         if (target.matches('.full-list')) {
-            popupBlock.style.visibility = 'visible';
+            popupRepair.style.visibility = 'visible';
+            document.body.style.overflow = 'hidden';
+        }
+        if (target.matches('.link-privacy')) {
+            popupPrivacy.style.visibility = 'visible';
+            document.body.style.overflow = 'hidden';
         }
         if (target.matches('.close')) {
-            popupBlock.style.visibility = 'hidden';
+            popupType.style.visibility = 'hidden';
+            popupType.removeAttribute('style');
+            document.body.style.overflow = '';
+            document.body.removeAttribute('style')
         }
+    };
+    document.body.addEventListener('click', e => {
+        const target = e.target;
+        popups(popupPrivacy, target);
+        popups(popupRepair, target);
     });
 };
 export default popup;
